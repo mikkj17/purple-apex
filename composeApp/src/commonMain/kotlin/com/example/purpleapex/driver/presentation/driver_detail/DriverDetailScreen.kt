e48bf9716729964a97e2307f4726e4e501ca7ceb
Mikkel Møller Jensen <mikkj17@gmail.com>
2025-05-22 21:29:59 +0200

    fetch all driver data

--- composeApp/src/commonMain/kotlin/com/example/purpleapex/driver/presentation/driver_detail/DriverDetailScreen.kt
+++ composeApp/src/commonMain/kotlin/com/example/purpleapex/driver/presentation/driver_detail/DriverDetailScreen.kt
@@ -1,10 +1,24 @@
 package com.example.purpleapex.driver.presentation.driver_detail
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBackIosNew
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.koin.compose.viewmodel.koinViewModel
 
@@ -12,21 +26,68 @@ import org.koin.compose.viewmodel.koinViewModel
 @Composable
 fun DriverDetailScreenRoot(
     viewModel: DriverDetailViewModel = koinViewModel(),
+    onBackClick: () -> Unit,
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
-    DriverDetailScreen(state)
+    DriverDetailScreen(
+        state = state,
+        onAction = { action ->
+            when (action) {
+                is DriverDetailAction.OnBackClick -> onBackClick()
+                else -> Unit
+            }
+        }
+    )
 }
 
 @Composable
 private fun DriverDetailScreen(
     state: DriverDetailState,
+    onAction: (DriverDetailAction) -> Unit,
 ) {
-    Surface(
-        color = MaterialTheme.colorScheme.surface
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.background(MaterialTheme.colorScheme.tertiary),
     ) {
-        Text(
-            state.qualifyings.firstOrNull()?.name.toString(),
-            style = MaterialTheme.typography.headlineLarge
-        )
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.primary)
+        ) {
+            IconButton(
+                onClick = {
+                    onAction(DriverDetailAction.OnBackClick)
+                },
+            ) {
+                Icon(
+                    Icons.Rounded.ArrowBackIosNew,
+                    contentDescription = "Go back",
+                )
+            }
+        }
+
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 8.dp)
+
+        ) {
+            when {
+                state.isLoading -> CircularProgressIndicator()
+                state.errorMessage != null -> Text(
+                    text = state.errorMessage,
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.headlineSmall,
+                    color = MaterialTheme.colorScheme.error,
+                )
+
+                else -> Column(
+
+                ) {
+                    Text(text = state.driver?.familyName ?: "")
+                }
+            }
+        }
     }
 }
