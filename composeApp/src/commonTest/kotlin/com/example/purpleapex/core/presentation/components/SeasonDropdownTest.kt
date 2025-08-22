d9874f2f2e9b3bf95efd6497c61a7f6a3bef30f3
Mikkel Møller Jensen <mikkj17@gmail.com>
2025-08-22 19:04:07 +0200

    fix unit test

--- composeApp/src/commonTest/kotlin/com/example/purpleapex/core/presentation/components/SeasonDropdownTest.kt
+++ composeApp/src/commonTest/kotlin/com/example/purpleapex/core/presentation/components/SeasonDropdownTest.kt
@@ -1,19 +1,16 @@
 package com.example.purpleapex.core.presentation.components
 
-import androidx.compose.ui.test.ExperimentalTestApi
-import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.runComposeUiTest
+import androidx.compose.ui.test.*
 import assertk.assertThat
 import assertk.assertions.isEqualTo
-import kotlinx.datetime.Clock
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.todayIn
 import kotlin.test.Test
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
 
 class SeasonDropdownTest {
-    @OptIn(ExperimentalTestApi::class)
+    @OptIn(ExperimentalTestApi::class, ExperimentalTime::class)
     @Test
     fun `season dropdown expands and selects another year when clicked`() = runComposeUiTest {
         val currentYear = Clock.System.todayIn(TimeZone.UTC).year
