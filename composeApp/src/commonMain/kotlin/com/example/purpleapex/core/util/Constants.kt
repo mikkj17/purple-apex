f0d00b1ddd631d0b6bbd7c8a19a463f2ab86c110
Mikkel Møller Jensen <mikkj17@gmail.com>
2026-02-22 13:56:19 +0100

    update base url

--- composeApp/src/commonMain/kotlin/com/example/purpleapex/core/util/Constants.kt
+++ composeApp/src/commonMain/kotlin/com/example/purpleapex/core/util/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.purpleapex.core.util
 
 object Constants {
-    const val BASE_URL = "https://purple-apex.com/graphql"
+    const val BASE_URL = "https://api.purple-apex.com/graphql"
     const val FIRST_F1_SEASON = 1950
     const val LATEST_F1_SEASON = 2025
 }
